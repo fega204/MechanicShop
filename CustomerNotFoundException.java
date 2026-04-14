9a2e5517088a7040b3c8eb610e32d992e15c1220
Banjoaok <Adebanjo.esan@edu.sait.ca>
2026-04-14 09:31:40 -0600

    Delete CustomerNotFoundException.java

--- CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mechanic.model;
-
-public interface Schedulable {
-    String book(String date, String time);
-    String cancel(int appointmentId);
-    String reschedule(int appointmentId, String newDate, String newTime);
-}
