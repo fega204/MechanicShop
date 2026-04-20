384b8af955196afc2880b16b54191117a6d1f1dc
fega El-pupu <fega.eze18@gmail.com>
2026-04-20 02:25:15 -0600

    Delete MemberB_MechanicShopApp (1)/MechanicShopApp directory

--- MemberB_MechanicShopApp (1)/MechanicShopApp/src/com/mechanicshop/model/Schedulable.java
+++
@@ -1,8 +0,0 @@
-package com.mechanicshop.model;
-
-public interface Schedulable {
-
-    boolean book(int appointmentId);
-    boolean cancel(int appointmentId);
-    boolean reschedule(int appointmentId, String newDate, String newTime);
-}
